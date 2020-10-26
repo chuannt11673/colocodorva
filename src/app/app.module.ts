6ebb2542fd8df803de1c9e38252f65a9628598a5
Chuan Nguyen <Chuan.Nguyen@altsourcesoftware.com>
2020-10-26 15:11:11 +0700

    rm angular-auth-oidc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -10,22 +10,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { AuthModule, LogLevel, OidcConfigService } from 'angular-auth-oidc-client';
-
-export function configureAuth(oidcConfigService: OidcConfigService) {
-  return () =>
-      oidcConfigService.withConfig({
-          stsServer: 'http://localhost:5000',
-          redirectUrl: window.location.origin,
-          postLogoutRedirectUri: window.location.origin,
-          clientId: 'spa',
-          scope: 'openid profile WebAppAPI',
-          responseType: 'code',
-          silentRenew: true,
-          silentRenewUrl: `${window.location.origin}/silent-renew.html`,
-          logLevel: LogLevel.Debug,
-      });
-}
 
 @NgModule({
   declarations: [AppComponent],
@@ -35,20 +19,12 @@ export function configureAuth(oidcConfigService: OidcConfigService) {
     IonicModule.forRoot(),
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule,
-    AuthModule.forRoot()
+    HttpClientModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    OidcConfigService,
-    {
-      provide: APP_INITIALIZER,
-      useFactory: configureAuth,
-      deps: [OidcConfigService],
-      multi: true
-    }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/welcome/welcome.page.ts
+++ src/app/welcome/welcome.page.ts
@@ -2,7 +2,6 @@ import { AuthService } from './../_core/services/auth.service';
 import { NavController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-welcome',
@@ -14,14 +13,12 @@ export class WelcomePage implements OnInit {
   constructor(
     public nav: NavController,
     public authService: AuthService,
-    public inAppBrowser: InAppBrowser,
-    public oidcSecurityService: OidcSecurityService) { }
+    public inAppBrowser: InAppBrowser) { }
 
   ngOnInit() {
   }
 
   facebookLogin() {
-    this.oidcSecurityService.authorize();   
   }
 
   login() {
